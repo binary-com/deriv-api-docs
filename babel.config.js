a19dcf930677a2a9535013376af37d63aff500b6
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-12-12 10:47:41 +0100

    fix: fixing testing config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   presets: [
-    require.resolve("@docusaurus/core/lib/babel/preset"),
     "@babel/preset-env",
     "@babel/preset-react",
     "@babel/preset-typescript",
     "@linaria",
+    require.resolve("@docusaurus/core/lib/babel/preset"),
   ],
 };
--- src/components/HeroHeader/__tests__/HeroHeader.test.tsx
+++ src/components/HeroHeader/__tests__/HeroHeader.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react";
 import { HeroHeader } from "../HeroHeader";
-import '@testing-library/jest-dom'
+
+jest.mock('@linaria/core', () => ({
+    css: () => jest.fn(),
+}))
 
 describe('homepage HeroHeader', () => {
     it('should render HeroHeader', async () => {
