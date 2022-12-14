a4caa8a32354833c87a7a91520a8ab223dc3ae46
sanjam chhatwal <sanjam@deriv.com>
2022-12-14 15:25:49 +0530

    Added deriv themes to the repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,10 @@ const config = {
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: [
+            require.resolve("./src/css/custom.css"),
+            "./src/css/themes.css",
+          ],
         },
       }),
     ],
