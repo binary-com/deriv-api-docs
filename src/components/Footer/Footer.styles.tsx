10501bcf3d37a15be8c34bb97cbe93bce38953d0
sanjam chhatwal <sanjam@deriv.com>
2022-12-15 16:38:06 +0530

    Footer styling

--- src/components/Footer/Footer.styles.tsx
+++ src/components/Footer/Footer.styles.tsx
@@ -1,8 +1,18 @@
 import { css } from "@linaria/core";
 
 export const footerContainer = css`
-  background-color: #000000;
+  background-color: var(--ifm-font-color-base);
   display: flex;
   flex-direction: row;
   padding: 2rem;
+  justify-content: space-evenly;
+`;
+
+export const footerBody = css`
+display:flex
+flex-direction:column;
+align-items:center;
+`;
+export const link = css`
+  background-color: var(--ifm-color-danger);
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Button, Text } from "@deriv/ui";
-import { footerContainer } from "./Footer.styles";
+import { footerContainer, footerBody,link } from "./Footer.styles";
 
 export const Footer = () => {
   return (
     <div data-testid="hero-header" className={footerContainer}>
-      <div>
+      <div className={footerBody}>
         <Text
-          type="heading-3"
+          type="subtitle-1"
           bold
           css={{
             color: "White",
@@ -21,7 +21,7 @@ export const Footer = () => {
           type="paragraph-1"
           align="center"
           css={{
-            color: "White",
+            color: "$greyLight500",
             "font-weight": 400,
             "@mobile": { "text-align": "left" },
           }}
@@ -29,13 +29,15 @@ export const Footer = () => {
         >
           Discuss ideas and share solutions with developers worldwide.
         </Text>
-        <Button>
+        <Button
+        color="secondary"
+        >
             Join Our Community
         </Button>
       </div>
-      <div>
+      <div className={footerBody}>
         <Text
-        type="heading-3"
+        type="subtitle-1"
         bold
         css={{
           color: "White",
@@ -50,14 +52,16 @@ export const Footer = () => {
         type="paragraph-1"
         align="center"
         css={{
-          color: "White",
+          color:"$greyLight500",
           "font-weight": 400,
+          "display":"flex",
+          "flexWrap":"wrap",
           "@mobile": { "text-align": "left" },
         }}
         role="heading"
         aria-level={7}
         >
-            Email us at api-support@deriv.com api-support@deriv.com
+            Email us at <a className={link}> api-support@deriv.com</a>
             if you have any questions.
 
         </Text>
