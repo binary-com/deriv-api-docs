712deefed86d9e4465a9f38f95ca77da93d69e34
sanjam chhatwal <sanjam@deriv.com>
2022-12-15 19:27:38 +0530

    Build fix

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HeroHeader } from '../HeroHeader/HeroHeader';
 import styles from './styles.module.css';
-import { Footer } from '../Footer/footer';
+import { Footer } from '../Footer/Footer';
 
 export default function HomepageFeatures(): JSX.Element {
   return (
