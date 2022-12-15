f9b17ee293afb5731e98220f7dc62bd677291a1e
sanjam chhatwal <sanjam@deriv.com>
2022-12-15 12:53:36 +0530

    Added css preset file

--- src/components/HeroHeader/HeroHeader.styles.tsx
+++ src/components/HeroHeader/HeroHeader.styles.tsx
@@ -8,27 +8,27 @@ export const heroImageStyle = css`
         background-repeat: no-repeat;
         background-size: cover;
         width: 100%;
-        height: 488px;
+        height: 48.8rem;
         display: flex;
         justify-content: var(--text-align-center);
         align-items: var(--text-align-center);
-        padding-top: 40px;
+        padding-top: 4rem;
         
         @media screen and (max-width: 992px) {
-            margin-top: 47px;
+            margin-top: 4.7rem;
             padding-top: unset;
         }
 
-        @media screen and (min-width: 320px) and (max-width: 425px) {
+        @media screen and (min-width: 32rem) and (max-width: 42.5rem) {
             background-image: linear-gradient(rgba(0, 0, 0, 0.16), rgba(0, 0, 0, 0.16)),
                 url(/img/header-mobile.png);
             background-repeat: no-repeat;
             background-position: var(--text-align-center);
-            height: 328px;
+            height: 32.8rem;
         }
         
         @media screen and (min-width: 320px) and (max-width: 375px) {
-            height: 378px;
+            height: 37.8rem;
         }
     `
 
@@ -37,12 +37,12 @@ export const heroContainerStyle = css`
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        padding: 160px 0;
+        padding: 16rem 0;
         width: 100%;
 
-        @media screen and (max-width: 992px) {
+        @media screen and (max-width: 99.2rem) {
             width: 100%;
-            padding: 80px 40px;
+            padding: 8rem 4rem;
             align-items: baseline;
         }
     `
