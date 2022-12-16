ddce59d6360603df228bea314673d40317026579
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-12-16 14:27:20 +0100

    style: adding background color per even page section

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -3,16 +3,19 @@ import { ClientLibaries } from '../ClientLibraries/ClientLibraries';
 import { HeroHeader } from '../HeroHeader/HeroHeader';
 import { Benefits } from '../Benefits/Benefits';
 import { Footer } from '../Footer/Footer';
+import { IndexStyling } from './index.styles';
 import styles from './styles.module.css';
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div>
-        <HeroHeader />
-        <Benefits />
-        <ClientLibaries />
-        <Footer />
+        <IndexStyling>
+          <HeroHeader />
+          <ClientLibaries />
+          <Benefits />
+          <Footer />
+        </IndexStyling>
       </div>
     </section>
   );
