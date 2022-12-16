c7c4841c3f4500f7e4cec666eca3c9782bacc74c
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-12-16 15:31:15 +0100

    fix: fixing import

--- src/components/Footer/__tests__/Footer.test.tsx
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { cleanup, render, screen } from '@testing-library/react';
-import { Footer } from '../footer';
+import { Footer } from '../Footer';
 
 beforeEach(() => {
   render(<Footer />);
