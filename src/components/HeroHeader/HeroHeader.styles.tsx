e443d2fbfaf126d0e28bfdd95df46a5cf8f8d115
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-12-16 14:27:57 +0100

    refactor: updating some stuff after merge

--- src/components/HeroHeader/HeroHeader.styles.tsx
+++ src/components/HeroHeader/HeroHeader.styles.tsx
@@ -1,39 +1,39 @@
 import { styled } from '@linaria/react';
 
 export const HeroImageStyle = styled.div`
-    background-color: var(--brown-02);
-    background-image: linear-gradient(rgba(0, 0, 0, 0.16), rgba(0, 0, 0, 0.16)), url(/img/header.png);
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    width: 100%;
-    height: 488px;
-    display: flex;
-    justify-content: var(--text-align-center);
-    align-items: var(--text-align-center);
-    padding-top: 40px;
+  background-color: var(--brown-02);
+  background-image: linear-gradient(rgba(0, 0, 0, 0.16), rgba(0, 0, 0, 0.16)), url(/img/header.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 488px;
+  display: flex;
+  justify-content: var(--text-align-center);
+  align-items: var(--text-align-center);
+  padding-top: 40px;
 
-    @media screen and (max-width: 992px) {
-        margin-top: 47px;
-        padding-top: unset;
-    }
-    
-    @media screen and (min-width: 320px) and (max-width: 375px) {
-        height: 378px;
-    }
-`
+  @media screen and (max-width: 992px) {
+    margin-top: 47px;
+    padding-top: unset;
+  }
+
+  @media screen and (min-width: 320px) and (max-width: 375px) {
+    height: 378px;
+  }
+`;
 
 export const HeroContainerStyle = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 160px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 160px 0;
+  width: 100%;
+
+  @media screen and (max-width: 992px) {
     width: 100%;
-    
-    @media screen and (max-width: 992px) {
-        width: 100%;
-        padding: 80px 40px;
-        align-items: baseline;
-    }
-`
+    padding: 80px 40px;
+    align-items: baseline;
+  }
+`;
