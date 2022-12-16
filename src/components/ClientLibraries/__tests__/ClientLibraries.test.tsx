38732a78e491fd8071ffb337ce5a154e7b666f93
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2022-12-16 15:19:31 +0100

    fix: fixing failing tests

--- src/components/ClientLibraries/__tests__/ClientLibraries.test.tsx
+++ src/components/ClientLibraries/__tests__/ClientLibraries.test.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import { ClientLibaries } from '../ClientLibraries';
 
-jest.mock('@linaria/core', () => ({
-  css: () => jest.fn(),
-}));
-
 describe('ClientLibraries', () => {
-  it('should render properly', () => {
+  beforeEach(() => {
     render(<ClientLibaries />);
+  });
+
+  afterEach(cleanup);
+
+  it('should render properly', () => {
     const client_header = screen.getByTestId('client-header');
     expect(client_header).toBeInTheDocument();
   });
   it('should render title properly', () => {
-    render(<ClientLibaries />);
     const client_text = screen.getByRole('heading', { level: 1 });
     expect(client_text).toBeInTheDocument();
   });
-  it('should render  subtitle text properly', () => {
-    render(<ClientLibaries />);
+  it('should render subtitle text properly', () => {
     const client_subtitle = screen.getByRole('heading', { level: 4 });
     expect(client_subtitle).toBeInTheDocument();
   });
   it('should navigate to the correct links on click', () => {
-    render(<ClientLibaries />);
     expect(screen.getByText('Go to the JavaScript library').closest('a')).toHaveAttribute(
       'href',
       'https://binary-com.github.io/deriv-api/',
