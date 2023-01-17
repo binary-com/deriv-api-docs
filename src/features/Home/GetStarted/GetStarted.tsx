ddb75c268ee6b661cc0aa4edc2e0304cb4b08c54
sanjam chhatwal <sanjam@deriv.com>
2023-01-17 15:12:14 +0400

    fix: getstarted component

--- src/features/Home/GetStarted/GetStarted.tsx
+++ src/features/Home/GetStarted/GetStarted.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './GetStarted.module.scss';
+import { Text } from '@deriv/ui';
 export const GetStarted = () => {
   return (
-    <div className={`main-page-row ${styles.withPattern}`}>
-      <div className='column-container'>
-        <h1>Get started with our API in 3 simple steps:</h1>
-        <div className='card-container'>
+    <div className={`${styles.mainPageRow} ${styles.withPattern}`}>
+      <div className={styles.columnContainer}>
+        <Text type='heading-2' bold data-testid='started-header' role='heading'>
+          Get started with our API in 3 simple steps:
+        </Text>
+        <div className={styles.cardContainer}>
           <a
             target='_blank'
             href='https://deriv.com/signup/'
             rel='noopener noreferrer'
-            className='main-page-card'
+            className={styles.mainPageCard}
+            data-testid='signUp'
           >
-            <div className='header'>
-              <h3>1. Sign up</h3>
+            <div className={styles.header}>
+              <Text type='subtitle-1' bold>
+                1. Sign up
+              </Text>
               <div className={styles.signUpIcon} />
             </div>
             <div className='content hide-on-mobile'>
@@ -21,20 +27,21 @@ export const GetStarted = () => {
               <br />
               our API (or use your Binary.com login details).
             </div>
-            <div className='content hide-on-desktop'>
-              Create a free Deriv account to access our API (or use your Binary.com login details).
-            </div>
           </a>
-          <a href='/app-registration' className='main-page-card'>
-            <div className='header'>
-              <h3>2. Register your app</h3>
+          <a href='/app-registration' className={styles.mainPageCard} data-testid='register'>
+            <div className={styles.header}>
+              <Text type='subtitle-1' bold>
+                2. Register your app
+              </Text>
               <div className={styles.registerYourAppIcon} />
             </div>
             <div className='content'>Fill out the registration form to start using Deriv API.</div>
           </a>
-          <a href='/docs/resources/api-guide/' className='main-page-card'>
-            <div className='header'>
-              <h3>3. Read our guide</h3>
+          <a href='/docs/resources/api-guide/' className={styles.mainPageCard} data-testid='guide'>
+            <div className={styles.header}>
+              <Text type='subtitle-1' bold>
+                3. Read our guide
+              </Text>
               <div className={styles.guideIcon} />
             </div>
             <div className='content'>
---
+++ src/features/Home/GetStarted/__tests__/GetStarted.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { cleanup, render, screen } from '@site/src/test-utils';
+import { GetStarted } from '../GetStarted';
+
+describe('GetStarted', () => {
+  beforeEach(() => {
+    render(<GetStarted />);
+  });
+
+  afterEach(cleanup);
+
+  it('should render properly', () => {
+    const get_started = screen.getByTestId('started-header');
+    expect(get_started).toBeInTheDocument();
+  });
+  it('should render title properly', () => {
+    const started_header = screen.getByRole('heading');
+    expect(started_header).toHaveTextContent('Get started with our API in 3 simple steps:');
+  });
+  it('should navigate to the correct links on click', () => {
+    expect(screen.getByTestId('signUp').closest('a')).toHaveAttribute(
+      'href',
+      'https://deriv.com/signup/',
+    );
+    expect(screen.getByTestId('register').closest('a')).toHaveAttribute(
+      'href',
+      '/app-registration',
+    );
+    expect(screen.getByTestId('guide').closest('a')).toHaveAttribute(
+      'href',
+      '/docs/resources/api-guide/',
+    );
+  });
+});
