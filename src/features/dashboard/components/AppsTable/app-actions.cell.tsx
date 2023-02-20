64af826ab4855f9c192d90598d522aaad62ff2db
Mohsen Hajibeigloo <mohsen@re-work.dev>
2023-02-20 16:00:34 +0330

    test: added tests for apps table

--- src/features/dashboard/components/AppsTable/app-actions.cell.tsx
+++ src/features/dashboard/components/AppsTable/app-actions.cell.tsx
@@ -10,11 +10,19 @@ interface IAppActionsCellProps extends React.PropsWithChildren<CellProps<TAppCol
 
 const AppActionsCell = ({ openDeleteDialog, openEditDialog }: IAppActionsCellProps) => {
   return (
-    <div className={styles.appActions}>
-      <div onClick={openEditDialog} className={`${styles.updateApp} ${styles.tooltip}`}>
+    <div className={styles.appActions} data-testid={'app-action-cell'}>
+      <div
+        onClick={openEditDialog}
+        className={`${styles.updateApp} ${styles.tooltip}`}
+        data-testid={'update-app-button'}
+      >
         <span className={styles.tooltipText}>Edit application details</span>
       </div>
-      <div onClick={openDeleteDialog} className={`${styles.deleteApp} ${styles.tooltip}`}>
+      <div
+        onClick={openDeleteDialog}
+        className={`${styles.deleteApp} ${styles.tooltip}`}
+        data-testid={'delete-app-button'}
+      >
         <span className={styles.tooltipText}>Delete application</span>
       </div>
     </div>
