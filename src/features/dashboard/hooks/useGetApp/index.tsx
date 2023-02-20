c3e062ecd6541a8fe80963122de78772892f5bd7
Mohsen Hajibeigloo <mohsen@re-work.dev>
2023-02-20 17:12:37 +0330

    test: added tests for useGetApp

--- src/features/dashboard/hooks/useGetApp/index.tsx
+++ src/features/dashboard/hooks/useGetApp/index.tsx
@@ -2,13 +2,13 @@ import useWS from '@site/src/hooks/useWs';
 import { useCallback } from 'react';
 
 const useGetApps = () => {
-  const { send, data, is_loading, error } = useWS('app_list');
+  const { send, data, is_loading } = useWS('app_list');
 
   const getAllApps = useCallback(() => {
     send();
   }, [send]);
 
-  return { getAllApps, apps: data };
+  return { getAllApps, apps: data, is_loading };
 };
 
 export default useGetApps;
