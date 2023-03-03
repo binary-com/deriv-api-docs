5c9e90e9e6076c52b7a1e434c34a0c4f39569d9d
sanjam chhatwal <sanjam@deriv.com>
2023-03-03 10:43:02 +0400

    fix: build fix2

--- src/pages/apiexplorer.tsx
+++ src/pages/apiexplorer.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import ApiExplorerFeatures from '@site/src/features/ApiExplorer';
-import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function ApiExplorer() {
   return (
     <Layout title={'API Explorer'} description='Deriv API documentation'>
       <main>
-        <BrowserOnly>{() => <ApiExplorerFeatures />}</BrowserOnly>
+        <ApiExplorerFeatures />
       </main>
     </Layout>
   );
