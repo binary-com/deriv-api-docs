00c6c225794f36dbcc6e5c6e7cddcbebd370d573
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-03-21 12:27:56 +0100

    chore: adding currencyicon test

--- src/components/CurrencyIcon/index.tsx
+++ src/components/CurrencyIcon/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { CURRENCY_MAP } from '@site/src/utils';
 
 const CurrencyIcon = ({ currency }: { currency: string }) => {
-  const icon = CURRENCY_MAP?.get(currency)?.icon;
+  const icon = CURRENCY_MAP.get(currency) ? CURRENCY_MAP.get(currency).icon : 'placeholder_icon';
 
   return (
     <img
+      data-testid='currency-icon'
       alt={`${currency}-icon`}
       src={`/img/${icon}.svg`}
       onError={(e) => {
