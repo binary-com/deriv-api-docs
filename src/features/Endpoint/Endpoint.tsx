0f79ebb52c5ef15a4b2106475cba7837535a776a
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-03-21 10:05:17 +0400

    perf: variable change

--- src/features/Endpoint/Endpoint.tsx
+++ src/features/Endpoint/Endpoint.tsx
@@ -36,8 +36,8 @@ const EndPoint = () => {
     window.location.reload();
   };
 
-  const server_url = localStorage.getItem('config.server_url');
-  const app_id = localStorage.getItem('config.app_id');
+  const server_url = localStorage.getItem('config.server_url') ?? default_endpoint.server_url;
+  const app_id = localStorage.getItem('config.app_id') ?? default_endpoint.app_id;
   const current_url = `wss://${server_url}/websockets/v3?app_id=${app_id}&l=EN&brand=deriv`;
 
   return (
