3fd2ff3a1933a4398445aa5342164bc123aafa91
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-03-22 13:23:10 +0400

    fix: changed anchor tag to Link tag

--- src/features/Home/GetStarted/GetStarted.tsx
+++ src/features/Home/GetStarted/GetStarted.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './GetStarted.module.scss';
 import { Text } from '@deriv/ui';
+import Link from '@docusaurus/Link';
+
 export const GetStarted = () => {
   return (
     <div className={`${styles.mainPageRow} ${styles.withPattern}`}>
@@ -9,7 +11,7 @@ export const GetStarted = () => {
           Get started with our API in 3 simple steps:
         </Text>
         <div className={styles.cardContainer}>
-          <a href='/docs/resources/api-guide/' className={styles.mainPageCard} data-testid='guide'>
+          <Link to='/docs/resources/api-guide/' className={styles.mainPageCard} data-testid='guide'>
             <div className={styles.header}>
               <Text type='subtitle-1' bold className={styles.dark} as='h2'>
                 1. Learn about our API
@@ -17,10 +19,10 @@ export const GetStarted = () => {
               <div className={styles.guideIcon} />
             </div>
             <div className='content'>Understand basic concepts and terminologies.</div>
-          </a>
-          <a
+          </Link>
+          <Link
             target='_blank'
-            href='https://deriv.com/signup/'
+            to='https://deriv.com/signup/'
             rel='noopener noreferrer'
             className={styles.mainPageCard}
             data-testid='signUp'
@@ -36,8 +38,8 @@ export const GetStarted = () => {
               <br />
               our API (or use your Binary.com login details).
             </div>
-          </a>
-          <a href='/app-registration' className={styles.mainPageCard} data-testid='register'>
+          </Link>
+          <Link to='/app-registration' className={styles.mainPageCard} data-testid='register'>
             <div className={styles.header}>
               <Text type='subtitle-1' bold className={styles.dark} as='h2'>
                 3. Register your app
@@ -45,18 +47,18 @@ export const GetStarted = () => {
               <div className={styles.registerYourAppIcon} />
             </div>
             <div className='content'>Fill out the registration form to start using Deriv API.</div>
-          </a>
+          </Link>
         </div>
         <span className={styles.termConditions}>
           By using our API, you confirm that you have read and agreed to our
-          <a
-            href='https://deriv.com/tnc/business-partners-api-user.pdf'
+          <Link
+            to='https://deriv.com/tnc/business-partners-api-user.pdf'
             target='_blank'
             rel='noopener noreferrer'
           >
             {' '}
             terms and conditions.
-          </a>
+          </Link>
         </span>
       </div>
     </div>
