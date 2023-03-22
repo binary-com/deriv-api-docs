aa1b7f9bf64b11756104127d0d402f6a2bfd1b7f
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-03-22 13:31:51 +0100

    chore: removing console log and introduction from guides

--- src/features/dashboard/components/ApiTokenForm/api-token.form.tsx
+++ src/features/dashboard/components/ApiTokenForm/api-token.form.tsx
@@ -79,7 +79,6 @@ const ApiTokenForm = (props: HTMLAttributes<HTMLFormElement>) => {
         trade: data.trade,
         trading_information: data.trading_information,
       });
-      console.log(selectedTokenScope);
       createToken(name, selectedTokenScope);
     },
     [createToken],
