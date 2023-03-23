aa65a8791430cd2fb3838125a0bea7204a6123cc
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-03-23 12:31:36 +0100

    chore: adding test coverage

---
+++ src/components/CustomSelectDropdown/__tests__/CustomSelectDropdown.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import CustomSelectDropdown from '..';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const registerMock = jest.fn();
+
+describe('', () => {
+  it('should be able to render the component', () => {
+    render(
+      <CustomSelectDropdown label='test' value='test' register={registerMock()}>
+        <div>Selected item element</div>
+        <div>Dropdown element</div>
+      </CustomSelectDropdown>,
+    );
+
+    const custom_dropdown = screen.getByTestId('custom-dropdown');
+    expect(custom_dropdown).toBeInTheDocument();
+  });
+
+  it('should be able to render the component with an error message', () => {
+    render(
+      <CustomSelectDropdown label='test' value='test' register={registerMock()} is_error>
+        <div>Selected item element</div>
+        <div>Dropdown element</div>
+        <div>Error message</div>
+      </CustomSelectDropdown>,
+    );
+
+    const error_message = screen.getByText('Error message');
+    expect(error_message).toBeVisible();
+  });
+
+  it('should be able to show the dropdown when using hotkeys', async () => {
+    render(
+      <CustomSelectDropdown label='test' value='test' register={registerMock()}>
+        <div>Selected item element</div>
+        <div>Dropdown element</div>
+      </CustomSelectDropdown>,
+    );
+
+    await userEvent.keyboard('{Tab}{ArrowDown}');
+
+    const dropdown_list = screen.getByTestId('custom-dropdown');
+    expect(dropdown_list.classList.contains('active')).toBe(true);
+  });
+});
--- src/components/CustomSelectDropdown/index.tsx
+++ src/components/CustomSelectDropdown/index.tsx
@@ -36,6 +36,7 @@ const CustomSelectDropdown = ({
         onClick={() => setToggleDropdown(!is_toggle_dropdown)}
         onKeyDown={(e) => e.key === 'ArrowDown' && setToggleDropdown(!is_toggle_dropdown)}
         className={`${styles.selectWrapper} ${toggle_dropdown} ${is_error ? styles.error : ''}`}
+        data-testid='custom-dropdown'
       >
         <label className={`${styles.selectLabel} ${value ? styles.active : ''}`}>{label}</label>
         <SelectInput />
