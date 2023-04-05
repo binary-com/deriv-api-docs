4cd9117532ee7c4f310d566db517a777f6c67269
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-04-05 14:11:41 +0200

    chore: refactoring account switcher

--- src/components/AccountSwitcher/index.tsx
+++ src/components/AccountSwitcher/index.tsx
@@ -1,49 +1,28 @@
 import { Button } from '@deriv/ui';
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import useAuthContext from '@site/src/hooks/useAuthContext';
 import useLogout from '@site/src/hooks/useLogout';
-import useAccountSelector from '@site/src/hooks/useAccountSelector';
-import React from 'react';
+import AccountDropdown from '../CustomSelectDropdown/account-dropdown/AccountDropdown';
+import React, { useState } from 'react';
 import styles from './account_switcher.module.scss';
 
 const AccountSwitcher = () => {
-  const { loginAccounts, currentLoginAccount } = useAuthContext();
-  const { onSelectAccount } = useAccountSelector();
+  const { currentLoginAccount } = useAuthContext();
   const { logout } = useLogout();
+  const [dropdown_is_open, setDropdownIsOpen] = useState(false);
+  const dropdown_toggle = dropdown_is_open ? styles.active : styles.inactive;
 
   return (
     <>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger className='navbar__item navbar__link' asChild>
-          <Button type='button' color={'tertiary'}>
-            {currentLoginAccount.name && currentLoginAccount.currency
-              ? `${currentLoginAccount.name} - ${currentLoginAccount.currency}`
-              : 'Accounts'}
-          </Button>
-        </DropdownMenu.Trigger>
-
-        <DropdownMenu.Portal>
-          <DropdownMenu.Content
-            className={styles.DropdownMenuContent}
-            sideOffset={20}
-            alignOffset={-50}
-            align={'end'}
-          >
-            {loginAccounts.map((accountItem) => (
-              <DropdownMenu.Item
-                key={accountItem.name}
-                className={styles.DropdownMenuItem}
-                onSelect={() => {
-                  onSelectAccount(accountItem.name);
-                }}
-              >
-                {`${accountItem.name} - ${accountItem.currency}`}
-              </DropdownMenu.Item>
-            ))}
-            <DropdownMenu.Arrow className={styles.DropdownMenuArrow} />
-          </DropdownMenu.Content>
-        </DropdownMenu.Portal>
-      </DropdownMenu.Root>
+      <div className={styles.accountSwitcher}>
+        <button onClick={() => setDropdownIsOpen(!dropdown_is_open)}>
+          {currentLoginAccount.name && currentLoginAccount.currency
+            ? `${currentLoginAccount.name} - ${currentLoginAccount.currency}`
+            : 'Accounts'}
+        </button>
+        <div className={`${styles.accountDropdownContainer} ${dropdown_toggle}`}>
+          <AccountDropdown />
+        </div>
+      </div>
       <Button onClick={logout} type='button' color={'tertiary'}>
         Logout
       </Button>
