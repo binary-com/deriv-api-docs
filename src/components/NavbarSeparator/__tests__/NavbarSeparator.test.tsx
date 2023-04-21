50f7f047a02489cbec864227a985a49d61271fda
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-04-21 10:53:50 +0200

    chore: adding test coverage

--- src/components/NavbarSeparator/__tests__/NavbarSeparator.test.tsx
+++ src/components/NavbarSeparator/__tests__/NavbarSeparator.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NavbarSeparator from '..';
 import useAuthContext from '@site/src/hooks/useAuthContext';
 import { IAuthContext } from '@site/src/contexts/auth/auth.context';
-import { render, screen } from '@testing-library/react';
+import { findByTestId, render, screen } from '@testing-library/react';
 
 jest.mock('@site/src/hooks/useAuthContext');
 
@@ -18,5 +18,15 @@ describe('NavbarSeparator', () => {
 
     const separator = await screen.findByTestId('dt_navbar_separator');
     expect(separator).toBeVisible();
+  });
+
+  it('does not render separator when user is not logged in', () => {
+    mockUseAuthContext.mockImplementation(() => ({
+      is_logged_in: false,
+    }));
+    render(<NavbarSeparator />);
+
+    const separatorContainer = screen.queryByTestId('dt_navbar_separator');
+    expect(separatorContainer).not.toBeInTheDocument();
   });
 });
