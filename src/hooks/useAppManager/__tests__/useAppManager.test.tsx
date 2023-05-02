9ff895609f749db9b1d3decfaec7a1141112981e
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-05-02 15:53:23 +0400

    perf: add testcases

--- src/hooks/useAppManager/__tests__/useAppManager.test.tsx
+++ src/hooks/useAppManager/__tests__/useAppManager.test.tsx
@@ -1,12 +1,8 @@
-import makeMockSocket from '@site/src/__mocks__/socket.mock';
-import { act, renderHook, cleanup } from '@testing-library/react-hooks';
-import WS from 'jest-websocket-mock';
+import { act, renderHook } from '@testing-library/react-hooks';
 import React from 'react';
 import useAppManager from '..';
 import AuthProvider from '@site/src/contexts/auth/auth.provider';
 import AppManagerContextProvider from '@site/src/contexts/app-manager/app-manager.provider';
-
-const connection = makeMockSocket();
 
 const wrapper = ({ children }) => (
   <AuthProvider>
@@ -16,15 +12,6 @@ const wrapper = ({ children }) => (
 );
 
 describe('use App Manager', () => {
-  let wsServer: WS;
-  beforeEach(async () => {
-    wsServer = await connection.setup();
-  });
-  afterEach(async () => {
-    connection.tearDown();
-    cleanup();
-  });
-
   it('Should have is_dashboard falsy', () => {
     const { result } = renderHook(() => useAppManager(), { wrapper });
     expect(result.current.is_dashboard).toBeFalsy();
@@ -41,5 +28,13 @@ describe('use App Manager', () => {
       result.current.updateCurrentTab('REGISTER_APP');
     });
     expect(result.current.currentTab).toBe('REGISTER_APP');
+  });
+
+  it('Should set is_dashboard to truthy when user visits dashboard tab', () => {
+    const { result } = renderHook(() => useAppManager(), { wrapper });
+    act(() => {
+      result.current.setIsDashboard(true);
+    });
+    expect(result.current.is_dashboard).toBeTruthy();
   });
 });
