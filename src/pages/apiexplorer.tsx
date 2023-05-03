0f43c1b0a86e41c61d09e7f152aa7add15b3e9e4
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-05-03 14:10:46 +0200

    chore: trying to fix vercel error

--- src/pages/apiexplorer.tsx
+++ src/pages/apiexplorer.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import ApiExplorerFeatures from '../features/Apiexplorer';
-import { Circles } from 'react-loader-spinner';
 
 const ApiExplorer = () => {
   return (
-    <Layout title={'API Explorer'} description='Deriv API documentation'>
-      <main>
-        <ApiExplorerFeatures />
-      </main>
-    </Layout>
+    <BrowserOnly>
+      {() => (
+        <Layout title={'API Explorer'} description='Deriv API documentation'>
+          <main>
+            <ApiExplorerFeatures />
+          </main>
+        </Layout>
+      )}
+    </BrowserOnly>
   );
 };
 
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import { AppManager } from '@site/src/features/dashboard';
-import BrowserOnly from '@docusaurus/BrowserOnly';
-
 export default function AppRegistrationPage() {
   return (
-    <BrowserOnly>
-      {() => (
-        <Layout title='Dashboard' description='Manage your apps and api tokens'>
-          <main>
-            <AppManager />
-          </main>
-        </Layout>
-      )}
-    </BrowserOnly>
+    <Layout title='Dashboard' description='Manage your apps and api tokens'>
+      <main>
+        <AppManager />
+      </main>
+    </Layout>
   );
 }
