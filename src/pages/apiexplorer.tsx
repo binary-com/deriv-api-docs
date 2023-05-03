bb6e78a9709bb4d6a981ea656ce7ebf7f3681255
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-05-03 11:46:01 +0200

    chore: solving merge conflict

--- src/pages/apiexplorer.tsx
+++ src/pages/apiexplorer.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import ApiExplorerFeatures from '../features/Apiexplorer';
+import { Circles } from 'react-loader-spinner';
 
 const ApiExplorer = () => {
   return (
     <Layout title={'API Explorer'} description='Deriv API documentation'>
       <main>
-        <ApiExplorerFeatures />
+        <BrowserOnly
+          fallback={
+            <Circles
+              height='100'
+              width='100'
+              color='#d44c0d'
+              ariaLabel='circles-loading'
+              wrapperClass='loading'
+            />
+          }
+        >
+          {() => <ApiExplorerFeatures />}
+        </BrowserOnly>
       </main>
     </Layout>
   );
