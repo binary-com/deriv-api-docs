37995fef452da4e2da6d7ce275c28b54d9152f7d
sanjam chhatwal <sanjam@deriv.com>
2023-05-03 13:31:20 +0400

    chore: dynamocimporttests

--- src/hooks/dynamicImport/__tests__/useDynamicImport.test.tsx
+++ src/hooks/dynamicImport/__tests__/useDynamicImport.test.tsx
@@ -36,8 +36,93 @@ describe('useDynamicImportJSON', () => {
     act(() => {
       result.current.dynamicImportJSON(result.current.text_data.selected_value);
     });
-    expect(result.current.request_info).toEqual(
-      '{\n  "active_symbols": "brief",\n  "product_type": "basic"\n}',
-    );
+    expect(result.current.request_info).toEqual({
+      $schema: 'http://json-schema.org/draft-04/schema#',
+      additionalProperties: false,
+      auth_required: 0,
+      default: {
+        $schema: 'http://json-schema.org/draft-04/schema#',
+        additionalProperties: false,
+        auth_required: 0,
+        description:
+          'Retrieve a list of all currently active symbols (underlying markets upon which contracts are available for trading).',
+        properties: {
+          active_symbols: {
+            description: 'If you use `brief`, only a subset of fields will be returned.',
+            enum: ['brief', 'full'],
+            type: 'string',
+          },
+          landing_company: {
+            description:
+              '[Optional] If you specify this field, only symbols available for trading by that landing company will be returned. If you are logged in, only symbols available for trading by your landing company will be returned regardless of what you specify in this field.',
+            enum: [
+              'iom',
+              'malta',
+              'maltainvest',
+              'svg',
+              'virtual',
+              'vanuatu',
+              'champion',
+              'champion-virtual',
+            ],
+            type: 'string',
+          },
+          passthrough: {
+            description:
+              '[Optional] Used to pass data through the websocket, which may be retrieved via the `echo_req` output field.',
+            type: 'object',
+          },
+          product_type: {
+            description:
+              '[Optional] If you specify this field, only symbols that can be traded through that product type will be returned.',
+            enum: ['basic'],
+            type: 'string',
+          },
+          req_id: { description: '[Optional] Used to map request to response.', type: 'integer' },
+        },
+        required: ['active_symbols'],
+        title: 'Active Symbols (request)',
+        type: 'object',
+      },
+      description:
+        'Retrieve a list of all currently active symbols (underlying markets upon which contracts are available for trading).',
+      properties: {
+        active_symbols: {
+          description: 'If you use `brief`, only a subset of fields will be returned.',
+          enum: ['brief', 'full'],
+          type: 'string',
+        },
+        landing_company: {
+          description:
+            '[Optional] If you specify this field, only symbols available for trading by that landing company will be returned. If you are logged in, only symbols available for trading by your landing company will be returned regardless of what you specify in this field.',
+          enum: [
+            'iom',
+            'malta',
+            'maltainvest',
+            'svg',
+            'virtual',
+            'vanuatu',
+            'champion',
+            'champion-virtual',
+          ],
+          type: 'string',
+        },
+        passthrough: {
+          description:
+            '[Optional] Used to pass data through the websocket, which may be retrieved via the `echo_req` output field.',
+          type: 'object',
+        },
+        product_type: {
+          description:
+            '[Optional] If you specify this field, only symbols that can be traded through that product type will be returned.',
+          enum: ['basic'],
+          type: 'string',
+        },
+        req_id: { description: '[Optional] Used to map request to response.', type: 'integer' },
+      },
+      required: ['active_symbols'],
+      title: 'Active Symbols (request)',
+      type: 'object',
+    });
   });
 });
