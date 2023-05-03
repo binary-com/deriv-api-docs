9376218c1a9bf556caa97dd3a52d44cb43adf794
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-05-03 14:09:27 +0200

    chore: trying to fix vercel error

--- src/features/Apiexplorer/LoginDialog/index.tsx
+++ src/features/Apiexplorer/LoginDialog/index.tsx
@@ -2,7 +2,6 @@ import React, { useCallback } from 'react';
 import { Modal } from '@deriv/ui';
 import useLoginUrl from '@site/src/hooks/useLoginUrl';
 import { Button } from '@deriv/ui';
-import useIsBrowser from '@docusaurus/useIsBrowser';
 import styles from './LoginDialog.module.scss';
 
 type TLoginDialog = {
@@ -11,7 +10,6 @@ type TLoginDialog = {
 
 export const LoginDialog = ({ setToggleModal }: TLoginDialog) => {
   const { getUrl } = useLoginUrl();
-  const isBrowser = useIsBrowser();
 
   const onOpenChange = useCallback(
     (open: boolean) => {
@@ -28,32 +26,28 @@ export const LoginDialog = ({ setToggleModal }: TLoginDialog) => {
     location.assign('https://deriv.com/signup/');
   };
   return (
-    <React.Fragment>
-      {isBrowser && (
-        <Modal defaultOpen onOpenChange={onOpenChange}>
-          <Modal.Portal>
-            <div className='modal-overlay'>
-              <Modal.Overlay />
-              <Modal.PageContent
-                title={'Authorisation required'}
-                has_close_button
-                className={styles.wrapper}
-              >
-                <div className={styles.modal}>Log in or sign up to continue.</div>
-                <div className={styles.buttonWrapper}>
-                  <Button color='tertiary' onClick={handleSignUp} className={styles.btn}>
-                    Sign up
-                  </Button>
-                  <Button color='primary' onClick={handleClick} className={styles.btn}>
-                    Log in
-                  </Button>
-                </div>
-              </Modal.PageContent>
+    <Modal defaultOpen onOpenChange={onOpenChange}>
+      <Modal.Portal>
+        <div className='modal-overlay'>
+          <Modal.Overlay />
+          <Modal.PageContent
+            title={'Authorisation required'}
+            has_close_button
+            className={styles.wrapper}
+          >
+            <div className={styles.modal}>Log in or sign up to continue.</div>
+            <div className={styles.buttonWrapper}>
+              <Button color='tertiary' onClick={handleSignUp} className={styles.btn}>
+                Sign up
+              </Button>
+              <Button color='primary' onClick={handleClick} className={styles.btn}>
+                Log in
+              </Button>
             </div>
-          </Modal.Portal>
-        </Modal>
-      )}
-    </React.Fragment>
+          </Modal.PageContent>
+        </div>
+      </Modal.Portal>
+    </Modal>
   );
 };
 
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import { AppManager } from '@site/src/features/dashboard';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
 export default function AppRegistrationPage() {
   return (
-    <Layout title='Dashboard' description='Manage your apps and api tokens'>
-      <main>
-        <AppManager />
-      </main>
-    </Layout>
+    <BrowserOnly>
+      {() => (
+        <Layout title='Dashboard' description='Manage your apps and api tokens'>
+          <main>
+            <AppManager />
+          </main>
+        </Layout>
+      )}
+    </BrowserOnly>
   );
 }
