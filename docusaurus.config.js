481c53fec019c30d2453f60350827e28601475fb
sanjam chhatwal <sanjam@deriv.com>
2023-05-18 15:40:40 +0400

    feat: translation set up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,15 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'pr'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+      },
+      pr: {
+        label: 'Portuguese',
+      },
+    },
   },
 
   plugins: ['@docusaurus/theme-live-codeblock', 'docusaurus-plugin-sass'],
@@ -80,6 +88,10 @@ const config = {
           },
           {
             type: 'custom-user-navbar-item',
+            position: 'right',
+          },
+          {
+            type: 'localeDropdown',
             position: 'right',
           },
         ],
