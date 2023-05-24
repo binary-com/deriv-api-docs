8cc70281ac8e20a484413252e306cf47fc7f3fea
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-05-24 12:52:35 +0200

    chore: search feature WIP

--- src/components/UserNavbarItem/item.desktop.tsx
+++ src/components/UserNavbarItem/item.desktop.tsx
@@ -1,11 +1,26 @@
 import Link from '@docusaurus/Link';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AccountSwitcher from '../AccountSwitcher';
 import { IUserNavbarItemProps } from './item.types';
 import styles from './UserNavbarItem.module.scss';
 
 const UserNavbarDesktopItem = ({ authUrl, is_logged_in }: IUserNavbarItemProps) => {
+  const [toggle_search, setToggleSearch] = useState<boolean>(false);
+
+  // Close the search modal when pressing the escape button
+  useEffect(() => {
+    if (toggle_search) {
+      const closeSearch = (event) => {
+        if (event.key === 'Escape') {
+          setToggleSearch(false);
+        }
+      };
+      window.addEventListener('keydown', closeSearch);
+      return () => window.removeEventListener('keydown', closeSearch);
+    }
+  }, [toggle_search]);
+
   const logInButtonClasses = clsx(
     'navbar__item navbar__link',
     styles.UserNavbarItem,
@@ -20,14 +35,16 @@ const UserNavbarDesktopItem = ({ authUrl, is_logged_in }: IUserNavbarItemProps) 
   return is_logged_in ? (
     <AccountSwitcher />
   ) : (
-    <React.Fragment>
+    <nav className={`right-navigation ${toggle_search ? 'search-open' : 'search-closed'}`}>
       <Link to={authUrl} className={logInButtonClasses} target='_self'>
         Log in
       </Link>
       <Link to={'https://deriv.com/signup/'} className={signUpButtonClasses} target='_self'>
         Sign up
       </Link>
-    </React.Fragment>
+      <button onClick={() => setToggleSearch((prev) => !prev)}>O</button>
+      <div className='search-overlay' onClick={() => setToggleSearch(false)} />
+    </nav>
   );
 };
 
