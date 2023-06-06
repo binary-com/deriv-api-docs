5a6cdfcc6f9f046c911cb0c0d5540655227213bf
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-06-06 17:23:52 +0400

    style: modified design

--- src/theme/NavbarItem/LocaleDropdownNavbarItem/index.tsx
+++ src/theme/NavbarItem/LocaleDropdownNavbarItem/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { useAlternatePageUtils } from '@docusaurus/theme-common/internal';
-import { translate } from '@docusaurus/Translate';
 import { useLocation } from '@docusaurus/router';
 import DropdownNavbarItem from '@theme/NavbarItem/DropdownNavbarItem';
 import type { LinkLikeNavbarItemProps } from '@theme/NavbarItem';
@@ -34,27 +33,13 @@ export default function LocaleDropdownNavbarItem({
       autoAddBaseUrl: false,
       className:
         // eslint-disable-next-line no-nested-ternary
-        locale === currentLocale
-          ? // Similar idea as DefaultNavbarItem: select the right Infima active
-            // class name. This cannot be substituted with isActive, because the
-            // target URLs contain `pathname://` and therefore are not NavLinks!
-            // mobile
-            // ? 'menu__link--active' :
-            'dropdown__link--active'
-          : '',
+        locale === currentLocale ? 'dropdown__link--active' : '',
     };
   });
 
   const items = [...dropdownItemsBefore, ...localeItems, ...dropdownItemsAfter];
 
-  // Mobile is handled a bit differently
-  const dropdownLabel =
-    // mobile  ? translate({
-    //       message: 'Languages',
-    //       id: 'theme.navbar.mobileLanguageDropdown.label',
-    //       description: 'The label for the mobile language switcher dropdown',
-    //     }) :
-    localeConfigs[currentLocale].label;
+  const dropdownLabel = localeConfigs[currentLocale].label;
 
   return (
     <DropdownNavbarItem
