5bb97de7803cdf42344ed8c23cb6210ae5d1fa25
Hubert Koster <hubertkoster@Hubert-Kosters-MacBook-Pro-C02F1N65MD6M.local>
2023-06-08 15:27:19 +0200

    chore: fixing search function when logged in

---
+++ src/components/SearchButton/index.tsx
@@ -0,0 +1,66 @@
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import { useLocation } from '@docusaurus/router';
+import styles from './SearchButton.module.scss';
+
+type TSearchButton = {
+  setToggleSearch: Dispatch<SetStateAction<boolean>>;
+  toggle_search: boolean;
+};
+
+const SearchButton = ({ setToggleSearch, toggle_search }: TSearchButton) => {
+  const location = useLocation();
+
+  const closeSearchHotkey = (event) => {
+    const press_escape = event.key === 'Escape';
+    const press_cmd_and_k = event.metaKey && event.key === 'k';
+
+    if (press_escape) setToggleSearch(false);
+    if (press_cmd_and_k) setToggleSearch(false);
+  };
+
+  const openSearchHotkey = (event) => {
+    const press_cmd_and_k = event.metaKey && event.key === 'k';
+    if (press_cmd_and_k) setToggleSearch(true);
+  };
+
+  const focusSearchInput = () => {
+    // Using vanilla JS to get the element since it's a 3rd party library. I cannot access through React.
+    const search_input = document.querySelector('.navbar__search-input');
+    if (search_input) {
+      const focusInput = () => (search_input as HTMLElement).focus();
+      focusInput();
+    }
+  };
+
+  useEffect(() => {
+    setToggleSearch(false);
+  }, [location]);
+
+  useEffect(() => {
+    if (toggle_search) {
+      focusSearchInput();
+      window.addEventListener('keydown', closeSearchHotkey);
+      return () => window.removeEventListener('keydown', closeSearchHotkey);
+    } else {
+      window.addEventListener('keydown', openSearchHotkey);
+      return () => window.removeEventListener('keydown', openSearchHotkey);
+    }
+  }, [toggle_search]);
+
+  return (
+    <React.Fragment>
+      <button
+        onClick={() => setToggleSearch((prev) => !prev)}
+        className={styles.searchButton}
+        data-testid='dt_search_button'
+      />
+      <div
+        className='search-overlay'
+        onClick={() => setToggleSearch(false)}
+        data-testid='dt_search_overlay'
+      />
+    </React.Fragment>
+  );
+};
+
+export default SearchButton;
--- src/components/UserNavbarItem/item.desktop.tsx
+++ src/components/UserNavbarItem/item.desktop.tsx
@@ -1,51 +1,13 @@
 import Link from '@docusaurus/Link';
 import clsx from 'clsx';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import AccountSwitcher from '../AccountSwitcher';
 import { IUserNavbarItemProps } from './item.types';
-import { useLocation } from '@docusaurus/router';
 import styles from './UserNavbarItem.module.scss';
+import SearchButton from '../SearchButton';
 
 const UserNavbarDesktopItem = ({ authUrl, is_logged_in }: IUserNavbarItemProps) => {
   const [toggle_search, setToggleSearch] = useState<boolean>(false);
-  const location = useLocation();
-
-  const closeSearchHotkey = (event) => {
-    const press_escape = event.key === 'Escape';
-    const press_cmd_and_k = event.metaKey && event.key === 'k';
-
-    if (press_escape) setToggleSearch(false);
-    if (press_cmd_and_k) setToggleSearch(false);
-  };
-
-  const openSearchHotkey = (event) => {
-    const press_cmd_and_k = event.metaKey && event.key === 'k';
-    if (press_cmd_and_k) setToggleSearch(true);
-  };
-
-  const focusSearchInput = () => {
-    // Using vanilla JS to get the element since it's a 3rd party library. I cannot access through React.
-    const search_input = document.querySelector('.navbar__search-input');
-    if (search_input) {
-      const focusInput = () => (search_input as HTMLElement).focus();
-      focusInput();
-    }
-  };
-
-  useEffect(() => {
-    setToggleSearch(false);
-  }, [location]);
-
-  useEffect(() => {
-    if (toggle_search) {
-      focusSearchInput();
-      window.addEventListener('keydown', closeSearchHotkey);
-      return () => window.removeEventListener('keydown', closeSearchHotkey);
-    } else {
-      window.addEventListener('keydown', openSearchHotkey);
-      return () => window.removeEventListener('keydown', openSearchHotkey);
-    }
-  }, [toggle_search]);
 
   const logInButtonClasses = clsx(
     'navbar__item navbar__link',
@@ -68,16 +30,7 @@ const UserNavbarDesktopItem = ({ authUrl, is_logged_in }: IUserNavbarItemProps) 
       <Link to={'https://deriv.com/signup/'} className={signUpButtonClasses} target='_blank'>
         Sign up
       </Link>
-      <button
-        onClick={() => setToggleSearch((prev) => !prev)}
-        className={styles.searchButton}
-        data-testid='dt_search_button'
-      />
-      <div
-        className='search-overlay'
-        onClick={() => setToggleSearch(false)}
-        data-testid='dt_search_overlay'
-      />
+      <SearchButton setToggleSearch={setToggleSearch} toggle_search={toggle_search} />
     </nav>
   );
 };
