805e6a286da3a0e952b83e1179a24553d5cb8f9f
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-07-06 11:21:04 +0400

    test: restrictions list comp

---
+++ src/features/dashboard/components/RestrictionsAppname/__tests__/restrictions-appname.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@site/src/test-utils';
+import RestrictionsAppname from '..';
+
+describe('Restrictions for App name', () => {
+  it('Should render the list', () => {
+    render(<RestrictionsAppname />);
+
+    const AppRestrictionList = screen.getByRole('list');
+    expect(AppRestrictionList).toBeInTheDocument();
+  });
+
+  it('Should display correct content inside list items', () => {
+    render(<RestrictionsAppname />);
+
+    const listItem1 = screen.getByText(
+      'Only alphanumeric characters with spaces, underscores, and hyphens are allowed.',
+    );
+    const listItem2 = screen.getByText('The name can contain up to 48 characters.');
+    const listItem3 = screen.getByText('Duplicate token names aren’t allowed.');
+    const listItem4 = screen.getByText(
+      'The name cannot contain “Binary”, “Deriv”, or similar words.',
+    );
+
+    expect(listItem1).toBeInTheDocument();
+    expect(listItem2).toBeInTheDocument();
+    expect(listItem3).toBeInTheDocument();
+    expect(listItem4).toBeInTheDocument();
+  });
+});
