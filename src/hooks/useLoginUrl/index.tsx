10d288aec02118e3dc2e0f35b77156d0a7257686
sanjam chhatwal <sanjam@deriv.com>
2023-08-25 16:19:49 +0400

    fix: vercel fix

--- src/hooks/useLoginUrl/index.tsx
+++ src/hooks/useLoginUrl/index.tsx
@@ -5,7 +5,7 @@ const useLoginUrl = () => {
   const getUrl = useCallback((language = 'en') => {
     const { appId, oauth } = getServerConfig();
     const pathname = window.location.pathname;
-    const route = pathname.replace(/\//g, '%2F');
+    const route = pathname.replace(/\//g, '%2F'); //replacement is done for backend to understand the route
 
     return generateLoginUrl(language, oauth, appId, route);
   }, []);
