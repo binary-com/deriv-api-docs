03e1368a5e84386f5b0873da75507193bf0e0819
sanjam chhatwal <sanjam@deriv.com>
2023-09-04 16:32:47 +0400

    fix: documentation fix

--- src/hooks/useLoginUrl/index.tsx
+++ src/hooks/useLoginUrl/index.tsx
@@ -1,14 +1,19 @@
 import { generateLoginUrl, getServerConfig } from '@site/src/utils';
 import { useCallback } from 'react';
+import { useLocation } from '@docusaurus/router';
 
 const useLoginUrl = () => {
-  const getUrl = useCallback((language = 'en') => {
-    const { appId, oauth } = getServerConfig();
-    const pathname = window.location.pathname;
-    const route = pathname.replace(/\//g, '%2F'); //replacement is done for backend to understand the route
+  const location = useLocation();
+  const getUrl = useCallback(
+    (language = 'en') => {
+      const { appId, oauth } = getServerConfig();
+      const pathname = window.location.pathname;
+      const route = pathname.replace(/\//g, '%2F'); //replacement is done for backend to understand the route
 
-    return generateLoginUrl(language, oauth, appId, route);
-  }, []);
+      return generateLoginUrl(language, oauth, appId, route);
+    },
+    [location],
+  );
 
   return { getUrl };
 };
