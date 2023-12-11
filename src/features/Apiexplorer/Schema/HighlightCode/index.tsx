12dab35d288e0c3362492dcd2d105c688bda9ef4
utkarsha-deriv <utkarsha@regentmarkets.com>
2023-12-11 17:43:31 +0400

    feat: add to link api call names in description - generalise

--- src/features/Apiexplorer/Schema/HighlightCode/index.tsx
+++ src/features/Apiexplorer/Schema/HighlightCode/index.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HighlightCode.module.scss';
+import { playground_requests } from '@site/src/utils/playground_requests';
 import { SchemaDescriptionTypes } from '../RecursiveContent/SchemaDescription';
+import { useHistory, useLocation } from '@docusaurus/router';
 
 export const HighlightCode = ({ description }: SchemaDescriptionTypes) => {
+  const { pathname } = useLocation();
+  const history = useHistory();
+
   if (!description) return null;
 
   const [first, code, ...rest] = description.split('`');
+
+  const api_call_object = playground_requests.find((el) => el.name === code);
+  const link_api_call = (obj) => {
+    window.scrollTo(0, 0);
+    history.push(`${pathname}#${obj?.name}`);
+  };
+
   return (
     <React.Fragment>
       {first}
-      {code && <span className={clsx(styles.schemaRole, styles.schemaCode)}>{code}</span>}
+      {code && (
+        <span className={clsx(styles.schemaRole, styles.schemaCode)}>
+          {api_call_object ? (
+            <button onClick={() => link_api_call(api_call_object)} className={styles.schemaLink}>
+              {code}
+            </button>
+          ) : (
+            code
+          )}
+        </span>
+      )}
       {rest.length > 0 && <HighlightCode description={rest.join('`')} />}
     </React.Fragment>
   );
